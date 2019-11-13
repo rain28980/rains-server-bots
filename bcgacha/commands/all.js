ff45e919111e7ae420f9f7b0f086955c8874f379
rain28980 <53416008+rain28980@users.noreply.github.com>
2019-11-13 07:40:49 -0600

    Update all.js

--- bcgacha/commands/all.js
+++ bcgacha/commands/all.js
@@ -1,4 +1,19 @@
 exports.run = (client, message, args) => {
 const bclotto = require("../bclotto.js")
-    message.channel.send(bclotto.soup())
+//convert name to emoji
+const x = bclotto.soup()
+const y = x.replace(/\*|_/gi, "").replace(/ /gi, "_")
+const z = client.emojis.find(emoji => emoji.name === y)
+//send embed
+    message.channel.send({embed: {
+    description: `${z} ${x}`,
+    color: 0x505050,
+    thumbnail: {
+      url: "https://cdn.discordapp.com/avatars/481973540317560832/1c20333f6e35bf6b84b1f2cef7b44b53.png"
+    },
+    author: {
+      name: "Battle Cats Gacha",
+      url: "https://top.gg/bot/481973540317560832"
+    }
+  }});
 }
