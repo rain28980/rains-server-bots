a9204aff18e07284fc6ef6bc8341f16643b863b0
rain28980 <53416008+rain28980@users.noreply.github.com>
2019-11-13 12:37:17 -0600

    Update all11.js

--- bcgacha/commands/all11.js
+++ bcgacha/commands/all11.js
@@ -1,11 +1,12 @@
 exports.run = (client, message, args) => {
 const bclotto = require("../bclotto.js")
 
-const accounts = [bclotto.soup(),bclotto.soup(),bclotto.soup(),bclotto.soup(),bclotto.soup(),bclotto.soup(),bclotto.soup(),bclotto.soup(),bclotto.soup(),bclotto.soup(),bclotto.soup()]
-      accounts[i].replace(/\*|_/gi, "").replace(/ /gi, "_")
-
+const a = [bclotto.soup(),bclotto.soup(),bclotto.soup(),bclotto.soup(),bclotto.soup(),bclotto.soup(),bclotto.soup(),bclotto.soup(),bclotto.soup(),bclotto.soup(),bclotto.soup()]
+    for(i = 0; i < a.length; i++){
+  const b[i] = a[i].replace(/\*|_/gi, "").replace(/ /gi, "_")
+}
     message.channel.send({embed: {
-    description: `${z} ${x}`,
+    description: `${a[1]} ${b[1]}`,
     color: 0x505050,
     thumbnail: {
       url: "https://cdn.discordapp.com/avatars/481973540317560832/1c20333f6e35bf6b84b1f2cef7b44b53.png"
